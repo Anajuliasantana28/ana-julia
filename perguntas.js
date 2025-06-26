6fb9f5363ae4ef6ff8ac0c485eebb5ed3cb6d6e8
Anajuliasantana28 <ana.francelino.almeida@escola.pr.gov.br>
2025-06-26 07:54:38 -0300

    26/06

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Conhecimento geral',
     'quantos dentes permanentes o ser humano tem ?',
-    '32 permanentes'
+    '32 permanentes.'
 )
 
 criaCartao(
     'Conhecimento geral',
-    'O que é cárie??',
+    'O que é cárie?',
     'A cárie é uma doença causada por bactérias que destroem o esmalte e a dentina do dente, devido à produção de ácido a partir do açúcar. '
 )
 
 criaCartao(
     'Conhecimento geral',
-    'Como são chamados alguns dos nossos dentes',
-    'incisivos, caninos, pré-molares e molares'
+    'Como são chamados alguns dos nossos dentes?',
+    'incisivos, caninos, pré-molares e molares.'
 )
 
 criaCartao(
     'Conhecimento geral',
-    'Quando começa a troca dos dentes de leite ',
-    'geralmente começa por volta dos 6 anos de idade e pode se estender até os 12 anos'
+    'Quando começa a troca dos dentes de leite? ',
+    'geralmente começa por volta dos 6 anos de idade e pode se estender até os 12 anos.'
 )
