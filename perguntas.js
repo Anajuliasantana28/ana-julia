51e585d585681ec1084cb1f45f2f166b80a754d0
Anajuliasantana28 <ana.francelino.almeida@escola.pr.gov.br>
2025-06-26 07:34:29 -0300

    2606

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
+    'Conhecimento geral',
     'quantos dentes permanentes o ser humano tem ?',
     '32 permanentes'
 )
 
 criaCartao(
-    'Geografia',
-    'pode?',
-    'A capital da França é Paris'
+    'Conhecimento geral',
+    'O que é cárie??',
+    'A cárie é uma doença causada por bactérias que destroem o esmalte e a dentina do dente, devido à produção de ácido a partir do açúcar. '
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Conhecimento geral',
+    'Como são chamados alguns dos nossos dentes',
+    'incisivos, caninos, pré-molares e molares'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Conhecimento geral',
+    'Quando começa a troca dos dentes de leite ',
+    'geralmente começa por volta dos 6 anos de idade e pode se estender até os 12 anos'
 )
